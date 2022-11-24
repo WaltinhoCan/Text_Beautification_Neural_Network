da5f042654a475e77bdb82c7cfd2515ac6442178
Waltinho <116035407+WaltinhoCan@users.noreply.github.com>
2022-11-24 19:31:18 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+def config():
+    image_size = 400, 100
+    words_qty = 10
+    img_fraction = 0.30
+    percent_variation = -5,5
+    return image_size, words_qty, img_fraction, percent_variation
