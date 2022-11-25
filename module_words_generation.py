853737e0709e54d8305a45ed62a270fcc9fc98ab
Waltinho <116035407+WaltinhoCan@users.noreply.github.com>
2022-11-25 12:19:16 -0300

    Update module_words_generation.py

--- module_words_generation.py
+++ module_words_generation.py
@@ -4,20 +4,23 @@ from english_words import english_words_set
 import numpy as np 
 from PIL import ImageFont, ImageDraw, Image 
 import glob
-from config import config
+import config
 
-image_size, words_qty, img_fraction, percent_variation = config()
+image_size = 400, 100
+words_qty = 10
+img_fraction = 0.30
+percent_variation = -5,5
 
 def image_process(image_size):
     h, w = image_size
     blank = 255*np.ones((h, w)).astype(np.uint8)
     image_pil = Image.fromarray(blank)
     draw = ImageDraw.Draw(image_pil)
-    return blank, image_pil, draw
+    return blank, image_pil, draw, h, w
 
 def fonts_resizer(palavra, img_fraction, w):
     fontsize = 1
-    fonts = glob.glob("/TTF/*")
+    fonts = glob.glob("/home/palestina/Desktop/TESTES/Imagens/fontes/*")
     font = random.choice(fonts)
     font_pil = ImageFont.truetype(font, fontsize)
     while font_pil.getsize(palavra)[0] < img_fraction*w:
@@ -27,9 +30,10 @@ def fonts_resizer(palavra, img_fraction, w):
     font_pil = ImageFont.truetype(font, fontsize)
     return font_pil
 
-def dimension(palavra, draw, image_size, font_pil, low_percent, high_percent):
-    w, h = image_size
-    aw = ah = random.randint(low_percent, high_percent)/100
+def dimension(palavra, draw, image_size, font_pil, percent_variation):
+    h, w = image_size
+    low, high = percent_variation
+    aw = ah = random.randint(low, high)/100
     textsize = draw.textsize(palavra, font= font_pil)
     textw = int((w - textsize[0])/2)
     texth = int((h - textsize[1])/2)
@@ -41,9 +45,15 @@ def dimension(palavra, draw, image_size, font_pil, low_percent, high_percent):
     coord = (x, y)
     return dim, coord
 
-def text(coord, dim, palavra, font_pil, image_pil, draw, words_qty):
+def text():
+    image_size, words_qty, img_fraction, percent_variation = config.variables()
     palavras = (random.sample(english_words_set, words_qty))
+    print(palavras)
     for palavra in palavras:
+        blank, image_pil, draw, h, w = image_process(image_size)
+        font_pil = fonts_resizer(palavra, img_fraction, w)
+        dim, coord = dimension(palavra, draw, image_size, font_pil, percent_variation)
+        print(dim, coord)
         draw.text(coord, palavra, font = font_pil, align="center")
         processed = cv2.cvtColor(np.array(image_pil), cv2.COLOR_RGB2BGR)
         cv2.resize(processed, dim)
